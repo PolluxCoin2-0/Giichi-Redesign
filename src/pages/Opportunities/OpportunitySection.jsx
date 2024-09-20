5e6ed49ccbab69d2e9d84496c8fc9fa93c7e8cc7
DurgeshY21 <durgeshy5226@gmail.com>
2024-09-20 16:30:34 +0530

    buttons added

--- src/pages/Opportunities/OpportunitySection.jsx
+++ src/pages/Opportunities/OpportunitySection.jsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
 import HeroImg from "../../assets/banner_img_home.png";
+import ContactPopUp from "../../comopnents/ContactPopUp";
+import GetQuote from "../../comopnents/GetQuote";
 import ProgressBar from "../../comopnents/ProgressBar";
 
 
@@ -9,6 +12,27 @@ const testData = [
   ];
 
 const OpportunitySection = () => {
+
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [isQuoteOpen, setIsQuoteOpen] = useState(false);
+
+  const handleButtonClick1 = () => {
+    setIsPopupOpen(true);
+  };
+
+  const handleClosePopup1 = () => {
+    setIsPopupOpen(false);
+  };
+
+  const handleButtonClick2 = () => {
+    setIsQuoteOpen(true);
+  };
+
+  const handleClosePopup2 = () => {
+    setIsQuoteOpen(false);
+  };
+
+
     return (
         <div className="bg-[#000000] px-32 pt-20 pb-20 flex flex-row justify-between w-full">
 
@@ -48,7 +72,11 @@ const OpportunitySection = () => {
     <div className="flex flex-row  space-x-5 mt-4">
               <div>
                   {/* Button */}
-            <button className="bg-[#569F42]   text-[#ffff] mt-8 button rounded-xl transition-transform duration-300 ease-out transform group-hover:-translate-x-1/2 relative z-10">
+            <button className={`bg-[#569F42]   text-[#ffff] mt-8 button rounded-xl transition-transform duration-300 ease-out transform group-hover:-translate-x-1/2 relative 
+                ${
+                isPopupOpen || isQuoteOpen ? "z-0" : "z-10"
+              }`}
+              onClick={handleButtonClick1}>
               <a
                 href="#_"
                 className="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-indigo-600 transition duration-400 ease-out shadow-md group"
@@ -79,7 +107,11 @@ const OpportunitySection = () => {
 
               <div>
                   {/* Button */}
-            <button className="bg-[#569F42]  text-[#ffff] mt-8 button rounded-xl transition-transform duration-300 ease-out transform group-hover:-translate-x-1/2 relative z-10">
+            <button className={`bg-[#569F42]  text-[#ffff] mt-8 button rounded-xl transition-transform duration-300 ease-out transform group-hover:-translate-x-1/2 relative 
+                ${
+                isPopupOpen || isQuoteOpen ? "z-0" : "z-10"
+              }`}
+              onClick={handleButtonClick2}>
               <a
                 href="#_"
                 className="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-indigo-600 transition duration-400 ease-out shadow-md group"
@@ -109,6 +141,13 @@ const OpportunitySection = () => {
               </div>
              </div>
            
+           {/* Contact PopUp */}
+        {isPopupOpen && (
+          <ContactPopUp onClose={handleClosePopup1} className="z-50" />
+        )}
+        {isQuoteOpen && (
+          <GetQuote onClose={handleClosePopup2} className="z-50" />
+        )}
             </div>
 
         
